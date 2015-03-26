50dbda2365e138ebe7002f2dab9dc719a96384be
tiagoamemiya <tiagoamemiya@gmail.com>
2015-03-25 21:03:31 -0300

    Arquivos do virtualenv e app com exemplos simples

---
+++ app.py
@@ -0,0 +1,19 @@
+"""
+Name: Simple Blog
+Desc: Flask App
+"""
+from flask import Flask, url_for, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def view_home():
+	return 'Blog Home Page'
+
+@app.route('/post/<post_id>')
+def view_post(post_id):
+	return 'Post %s' % post_id
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run()
